068c4921e965bec1434a5ab0687e8d962bda1500
cassiadev <37288667+cassiadev@users.noreply.github.com>
2023-03-05 19:21:10 +0900

    Refined text explaining _data

--- app/src/main/java/com/example/newsscraper/MainViewModel.kt
+++ app/src/main/java/com/example/newsscraper/MainViewModel.kt
@@ -5,10 +5,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-    private val _data = mutableStateOf("Data to be shown")
+    private val _data = mutableStateOf("Data to be scraped in")
     val data: State<String> = _data
 
     fun startScraping() {
-        _data.value = "Data to be scraped in"
+        _data.value = "Value of data to be scraped in"
     }
 }
